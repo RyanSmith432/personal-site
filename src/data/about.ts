bd3c251775542f0d5a14f1f985211bf1b0df5677
Ryan Smith <crsmith026@gmail.com>
2026-07-03 13:15:44 -0600

    initial setup

--- src/data/about.ts
+++ src/data/about.ts
@@ -9,8 +9,8 @@ In my spare time, I enjoy focusing on projects with high operational impact. If 
 # Some History
 
 - My parents bought our first computer.
-- We subscribed to America Online (later AOL) in 1993.
-- At 18, I
+- We subscribed to America Online (later AOL) in 1995. I had to click on everything to figure out how to use it.
+- At 17, I started an intermship at MicronPC, where I learned to build computers and troubleshoot hardware issues.
 - At 22, I became a father of one Son.
 - At 38, I became a father of my 1st Daughter & 2nd child.
 - At 41, I became a father of my 2nd Daughter & 3rd child.
@@ -20,16 +20,17 @@ Ask me in person for other stories that I'm afraid to share with the internet.
 
 # I Like
 
-- Track days (Sportbikes).
-- Skiing.
+- [Track days (Sportbikes)](https://www.youtube.com/watch?v=70vUUoF3ie4).
+- [Skiing](https://www.youtube.com/watch?v=V8oZmw7POKM).
 - Mountain Biking.
+- [Road Biking](https://www.youtube.com/watch?v=x3k9fusy2zE).
 - Space.
 - Summer.
 - [Books](https://www.goodreads.com/).
 
 # Travel / Geography
 
-- I am originally from Anchorage, Alaska. I have since lived in Boise, San Mateo, & Half Moon Bay.
+- I am originally from Anchorage, Alaska. I have since lived in Huston, Redwood Shores, Half Moon Bay, & Boise.
 
 
 # Fun Facts
