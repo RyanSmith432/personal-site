000697f5b08ef2e7a69c5d66af9a4a8255ea0705
Ryan Smith <crsmith026@gmail.com>
2026-07-07 12:51:31 -0600

    Adding projects and images to projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,10 +17,11 @@ const data: Project[] = [
       '/images/projects/VMware_ESXi_v7_v8.webp',
       '/images/projects/VMware_VCSA_v7_v8.webp',
       '/images/projects/VMware_VCSA_Backups.webp',
-      '/images/projects/VMware_VCSA_Cert.webp'
+      '/images/projects/VMware_VCSA_Cert.webp',
+      '/images/projects/VMware_ESXi_v7_v8_Upgrade_Schedule.webp'
     ],
     date: '2022-10-15',
-    desc: 'Upgraded 1300+ ESXi hosts from v7 to v8, & VCSA servers from v7 to v8, ensuring compatibility with the latest features and security updates.',
+    desc: 'I lead the upgrade of 1300+ ESXi hosts from v7 to v8, & VCSA servers from v7 to v8, ensuring compatibility with the latest features and security updates. Including setting the schedule & creating the process documentation.',
     tech: ['VMware ESXi', 'vSphere', 'PowerCLI', 'Confluence'],
     featured: true,
   },
@@ -58,7 +59,21 @@ const data: Project[] = [
     tech: ['VMware', 'vSphere', 'PowerCLI', 'Confluence'],
     featured: true,
   },
-    {
+  {
+    title: 'OS Hardening and Security Compliance',
+    subtitle: 'Managed a project to reach Security teams 90% compliance requirement for Windows and Linux. We exceeded the requirement & achieved 96%-100%.',
+    images: [
+      '/images/projects/OS_Hardening_CIS_Compliance_GPOs.webp',
+      '/images/projects/OS_Hardening_CIS_Compliance_GPOs_02.webp',
+      '/images/projects/OS_Hardening_CIS_Compliance_GPO_Inheritance.webp',
+      '/images/projects/Tanium_How-To_Run_Assessments.webp'
+    ],
+    date: '2025-11-01',
+    desc: 'Lead the deployment & enforcement process. Creating, tuning & running the Tanium Assessments. As well as coordinating with App owners on the testing & impact of Security GPOs on Applications in a Development environment.',
+    tech: ['CIS', 'DORA', 'Tanium', 'RHEL', 'Server 2025', 'Server 2019', 'Group Policy', 'Active Directory'],
+    featured: true,
+  },
+  {
     title: 'Dell BIOS & OME upgrades for 1300+ Servers',
     subtitle: 'Upgrade Dell BIOS and OME servers to latest versions',
     images: [
@@ -71,8 +86,8 @@ const data: Project[] = [
     featured: true,
   },
   {
-    title: 'Image Management',
-    subtitle: 'Image Management and Deployments for Windows and Linux images including documentation.',
+    title: 'OS Image Management Automation',
+    subtitle: 'OS Image Management and Deployments for Windows and Linux images including documentation.',
     images: [
       '/images/projects/Image_Creation_Process.webp',
     ],
@@ -86,6 +101,7 @@ const data: Project[] = [
     subtitle: 'Deployed AD CS ESC1 & ESC8 Remediation',
     images: [
       '/images/projects/InTune - How-To_ AD CS ESC1 & ESC8 Remediation_v0.9.png',
+      '/images/projects/InTune - How-To_AD CS PKI_Enrollment.webp'
     ],
     date: '2024-10-15',
     desc: 'Managed the process documentation and deployment of AD CS ESC1 & ESC8 Remediations, ensuring enhanced security and compliance across the organization.',
@@ -123,6 +139,28 @@ const data: Project[] = [
     date: '2024-12-19',
     desc: 'Lead the deployment of NetApp BlueXP. As part of a larger initiative to delete unused files and reduce storage costs ahead of a data Migration project.',
     tech: ['NetApp ONTAP', 'NetApp BlueXP', 'Security'],
+    featured: true,
+  },
+  {
+    title: 'PURE Storage Plugin',
+    subtitle: 'Install PURE Storage Plugin on VMware VCSA.',
+    images: [
+      '/images/projects/PURE_plugin_VCSA_Install.webp',
+    ],
+    date: '2023-05-01',
+    desc: 'Lead the efforts to deploy the PURE Storage Plugin to VMware VSCAs.',
+    tech: ['VMware VCSA', 'VMware', 'PURE'],
+    featured: true,
+  },
+  {
+    title: 'UCS Fibric Interconnect port Enable/Disable How-To',
+    subtitle: 'Create a How-To for junior Engineers',
+    images: [
+      '/images/projects/UCS_FI_Port_Enable_or_Disable.webp',
+    ],
+    date: '2021-04-01',
+    desc: 'Created a How-To for Junior Engineers to follow that details the process of Enabling or Disabling a UCS Fabric Interconnect port.',
+    tech: ['Cisco', 'UCS', 'Cisco UCS'],
     featured: true,
   },
 ];
